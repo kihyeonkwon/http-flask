a56ba826f12446e2b4240b1490c89340ea374711
kihyeonkwon <kihyeon.kwn@gmail.com>
2021-12-06 16:50:57 +0900

    Add login required decorator

--- api/app.py
+++ api/app.py
@@ -1,10 +1,33 @@
-from flask import Flask, jsonify, request, current_app, Response
+from flask import Flask, jsonify, request, current_app, Response, g
 from flask.json import JSONEncoder
 from sqlalchemy import create_engine, text
 import bcrypt
 import jwt
 from datetime import datetime, timedelta
 from functools import wraps
+
+
+
+def get_user(user_id):
+    user = current_app.database.execute(text("""
+    SELECT
+    id,
+    name,
+    email,
+    profile
+    FROM users 
+    WHERE id = :user_id
+    """), {
+        'user_id':user_id
+    }).fetchone()
+
+    return {
+        'id':user['id'],
+        'name':user['name'],
+        'email':user['email'],
+        'profile':user['profile']
+        } if user else None
+
 
 
 def create_app(test_config = None):
@@ -90,14 +113,42 @@ def create_app(test_config = None):
         
 
 
+
+    def login_required(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            access_token = request.headers.get('Authorization')
+            if access_token :
+                try :
+                    payload = jwt.decode(access_token, current_app.config['JWT_SECRET_KEY'], 'HS256')
+                except jwt.InvalidTokenError:
+                    payload = None
+
+                if payload is None : return Response(status=401)
+
+                user_id = payload['user_id']
+                g.user_id = user_id
+                g.user = get_user(user_id) if user_id else None
+            else:
+                return Response(status = 401)
+
+
+            return f(*args, **kwargs)
+        return decorated_function
+
+
+
+
     
 
 
 
     @app.route('/tweet', methods=['POST'])
+    @login_required
     def tweet():
         user_tweet = request.json
         tweet = user_tweet['tweet']
+        user_tweet['id'] = g.user_id
 
         if len(tweet) > 300:
             return '300자를 초과했습니다', 400
@@ -132,6 +183,7 @@ def create_app(test_config = None):
         return jsonify(created_tweet)
 
     @app.route("/follow", methods=["POST"])
+    @login_required
     def follow():
         payload = request.json
         
@@ -158,7 +210,9 @@ def create_app(test_config = None):
         return jsonify({'user_id':user_id, 'follows':follows})
 
 
+
     @app.route("/unfollow", methods=["POST"])
+    @login_required
     def unfollow():
         payload = request.json
 
@@ -184,6 +238,7 @@ def create_app(test_config = None):
 
 
     @app.route("/timeline/<int:user_id>", methods=['GET'])
+    @login_required
     def timeline(user_id):
         payload = request.json
 
